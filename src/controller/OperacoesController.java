69ed444578108363ec152a926d2f2e487509c7d5
erick <erick@DEKU-PC.Home>
2023-02-15 14:52:41 -0300

    Atualização

--- src/controller/OperacoesController.java
+++ src/controller/OperacoesController.java
@@ -6,31 +6,31 @@ public class OperacoesController {
 		super();
 	}
 	//Exercício 1 Recursivo
-	public int Ex1(int x,int y) {
+	public int Mult(int x,int y) {
 	//Condição de parada
 		if(y == 0) {
 			return 0;	
 		} else {
-			return x + Ex1(x, y-1);
+			return x + Mult(x, y-1);
 		}
 	}
 	//Exercício 2 Recursivo
-	public int Ex2(int a, int b) {
+	public int DivResto(int a, int b) {
 	//Condição de parada 
 		if (a < b) {
 			return a;
 		}else{
-			return Ex2(a-b, b);
+			return DivResto(a-b, b);
 		}
 	}
 	//Exercício 3 Recursivo
-	public int Ex3(int[] V, int x) {
+	public int VetPares(int[] V, int x) {
 	//Condição de parada
 		if(x < 0) {
 			return 0;
 		}else if(V[x] % 2 == 0) {
-			return 1 + Ex3(V, x - 1);
+			return 1 + VetPares(V, x - 1);
 		}
-		return Ex3(V, x - 1);
+		return VetPares(V, x - 1);
 	}
 }
--- src/view/Principal.java
+++ src/view/Principal.java
@@ -16,21 +16,21 @@ public class Principal {
 			JOptionPane.showMessageDialog(null, "Teste será feito com os Números 3 (A) e 3 (B)");
 			int a = 3;
 			int b = 3;
-			JOptionPane.showMessageDialog(null, "Resultado da Multiplicação ==> "+op.Ex1(a, b));
+			JOptionPane.showMessageDialog(null, "Resultado da Multiplicação ==> "+op.Mult(a, b));
 			break;
 			//Chamada do Exercício 2
 			case 2:
 			JOptionPane.showMessageDialog(null, "Teste será feito com os Números 13 (Dividendo) e 5 (Divisor)");
 			int Divid = 13;
 			int Divis = 5;
-			JOptionPane.showMessageDialog(null, "Resultado do Resto da Divisão ==> "+op.Ex2(Divid, Divis));
+			JOptionPane.showMessageDialog(null, "Resultado do Resto da Divisão ==> "+op.DivResto(Divid, Divis));
 			break;
 			//Chamada do Exercício 3
 			case 3:
 			int[] Vet = {1,2,3,4,5};
 			int Len = Vet.length;
 			JOptionPane.showMessageDialog(null,  "Teste será feito com um Vetor de 5 posições de Num. Inteiros -> {1,2,3,4,5}");
-			JOptionPane.showMessageDialog(null, "Quantidade de Num. Pares ==> "+op.Ex3(Vet, Len - 1));
+			JOptionPane.showMessageDialog(null, "Quantidade de Num. Pares ==> "+op.VetPares(Vet, Len - 1));
 			break;
 			//Fecha o Menu
 			case 9: JOptionPane.showMessageDialog(null, "FIM");
