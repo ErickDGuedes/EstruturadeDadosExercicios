62bdd15e049459e4ee9c833e284a1c582bd400e6
erick <erick@DEKU-PC.Home>
2023-02-15 11:40:20 -0300

    Atualização

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -13,25 +13,24 @@ public class Principal {
 			switch(opc) {
 			//Chamada do Exercício 1
 			case 1:
-			int a = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor A: "));
-			int b = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor B: "));
-			JOptionPane.showMessageDialog(null, "Resultado ==> "+op.Ex1(a, b));
+			JOptionPane.showMessageDialog(null, "Teste será feito com os Números 3 (A) e 3 (B)");
+			int a = 3;
+			int b = 3;
+			JOptionPane.showMessageDialog(null, "Resultado da Multiplicação ==> "+op.Ex1(a, b));
 			break;
 			//Chamada do Exercício 2
 			case 2:
 			JOptionPane.showMessageDialog(null, "Teste será feito com os Números 13 (Dividendo) e 5 (Divisor)");
 			int Divid = 13;
 			int Divis = 5;
-			JOptionPane.showMessageDialog(null, "Resultado ==> "+op.Ex2(Divid, Divis));
+			JOptionPane.showMessageDialog(null, "Resultado do Resto da Divisão ==> "+op.Ex2(Divid, Divis));
 			break;
 			//Chamada do Exercício 3
 			case 3:
-			int[] Vet = new int [5];
+			int[] Vet = {1,2,3,4,5};
 			int Len = Vet.length;
-			for(int i = 0; i < Vet.length; i++) {
-				Vet[i] = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor do Índice "+i+" :"));			
-			}
-			JOptionPane.showMessageDialog(null, "Resultado ==> "+op.Ex3(Vet, Len - 1));
+			JOptionPane.showMessageDialog(null,  "Teste será feito com um Vetor de 5 posições de Num. Inteiros -> {1,2,3,4,5}");
+			JOptionPane.showMessageDialog(null, "Quantidade de Num. Pares ==> "+op.Ex3(Vet, Len - 1));
 			break;
 			//Fecha o Menu
 			case 9: JOptionPane.showMessageDialog(null, "FIM");
